ebab87d1d1482b2681cd255316acdb730e647e24
Enoch Whitaker <enochwhitaker3@gmail.com>
2023-08-31 11:14:19 -0600

    versioning chapter commit

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -1,5 +1,7 @@
-using Microsoft.AspNetCore.Cors;
+﻿using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc(
+        "v1",
+        new OpenApiInfo { Title = "MyBGList", Version = "v1.0" });
+    options.SwaggerDoc(
+        "v2",
+        new OpenApiInfo { Title = "MyBGList", Version = "v2.0" });
+});
 
 builder.Services.AddCors(options => {
     options.AddDefaultPolicy(cfg => {
@@ -24,13 +33,32 @@ builder.Services.AddCors(options => {
         });
 });
 
+builder.Services.AddApiVersioning(options => {
+    options.ApiVersionReader = new UrlSegmentApiVersionReader();  
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+});
+
+builder.Services.AddVersionedApiExplorer(options => {
+    options.GroupNameFormat = "'v'VVV";                           
+    options.SubstituteApiVersionInUrl = true;                     
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint(
+            $"/swagger/v1/swagger.json",
+            $"MyBGList v1");
+        options.SwaggerEndpoint(
+            $"/swagger/v2/swagger.json",
+            $"MyBGList v2");
+    });
 }
 
 if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))   
@@ -45,17 +73,25 @@ app.UseCors("AnyOrigin");
 app.UseAuthorization();
 
 
-app.MapGet("/error",
+app.MapGet("/v{version:ApiVersion}/error",
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
     [EnableCors("AnyOrigin")]
-[ResponseCache(NoStore = true)] () =>
+    [ResponseCache(NoStore = true)] () =>
     Results.Problem());
-app.MapGet("/error/test",
+
+app.MapGet("/v{version:ApiVersion}/error/test",
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
     [EnableCors("AnyOrigin")]
-[ResponseCache(NoStore = true)] () =>
+    [ResponseCache(NoStore = true)] () =>
     { throw new Exception("test"); });
-app.MapGet("/cod/test",
+
+app.MapGet("/v{version:ApiVersion}/cod/test",
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
     [EnableCors("AnyOrigin")]
-[ResponseCache(NoStore = true)] () =>
+    [ResponseCache(NoStore = true)] () =>
     Results.Text("<script>" +
         "window.alert('Your client supports JavaScript!" +
         "\\r\\n\\r\\n" +
